a26a2976405e94a29ed3ae995bda470d9ac36bde
jvenicius <vidivinny8859@gmail.com>
2022-07-23 08:47:18 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import AddCircleOutlinedIcon from "@mui/icons-material/AddCircleOutlined";
+import { Container, Typography } from "@mui/material";
+import Formulario from "./components/Formulario";
+import "fontsource-roboto";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm">
+      <Typography variant="h3" component="h1" align="center">
+        Projeto Anotações
+      </Typography>
+      <AddCircleOutlinedIcon />
+      <Formulario aoEnviar={aoEnviarForm} />
+    </Container>
   );
+}
+
+function aoEnviarForm(dados) {
+  console.log(dados);
 }
 
 export default App;
---
+++ src/components/Formulario/Formulario.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { TextField, Button, Alert, Snackbar } from "@mui/material";
+
+export function Formulario({ aoEnviar }) {
+  const [titulo, setTitulo] = useState("");
+  const [conteudo, setConteudo] = useState("");
+  const [open, setOpen] = useState(false);
+  const [erro, setErro] = useState(false);
+
+  function handleClick() {
+    setOpen(!open);
+  }
+
+  return (
+    <>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          handleClick();
+          setTitulo("");
+          setConteudo("");
+          aoEnviar({ titulo, conteudo });
+        }}
+      >
+        <TextField
+          id="titulo"
+          value={titulo}
+          label="Título"
+          variant="outlined"
+          margin="normal"
+          onChange={(event) => setTitulo(event.target.value)}
+          onBlur={(event) => {
+            titulo.length <= 3 ? setErro(true) : setErro(false);
+          }}
+          error={erro}
+          helperText={erro === true ? "Insira pelo menos 3 caracteres" : ""}
+          fullWidth
+        />
+        <TextField
+          id="conteudo"
+          value={conteudo}
+          label="Descrição"
+          variant="outlined"
+          margin="normal"
+          multiline
+          rows={5}
+          onChange={(event) => setConteudo(event.target.value)}
+          fullWidth
+        />
+        <Button type="submit" variant="contained" color="primary">
+          Salvar
+        </Button>
+      </form>
+
+      <Snackbar
+        open={open}
+        autoHideDuration={6000}
+        onClose={handleClick}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <Alert
+          onClose={handleClick}
+          variant="outlined"
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          A anotação foi salva!
+        </Alert>
+      </Snackbar>
+    </>
+  );
+}
